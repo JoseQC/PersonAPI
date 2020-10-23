d5fb5e0ed6fe388a8bc5974275d8842a3ccdda95
joseqc28 <jossukeqc@gmail.com>
2020-10-23 16:19:38 -0500

    second commit

--- src/main/java/com/everis/treining/PersonsApiApplication.java
+++ src/main/java/com/everis/treining/PersonsApiApplication.java
@@ -3,9 +3,9 @@ package com.everis.treining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 public class PersonsApiApplication {
